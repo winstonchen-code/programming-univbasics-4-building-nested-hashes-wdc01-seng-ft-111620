52e192a33f816bf65f3b91df16736138c383ccdf
Winston Chen <winstonchen.wm@gmail.com>
2020-10-29 22:23:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,10 +3,11 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  {:railroads => {}}
-  {:pieces => 4}
-  {:rent_in_dollars => {}}
-  {:names => {}}
+  {:railroads => {
+    :pieces => 4,
+    :rent_in_dollars => {},
+    :names => {}
+  }}
 end
 
 def monopoly_with_third_tier
